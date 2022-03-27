3a85dcc7823fcd0b4f692a17e2b22e74c1178b81
Alexander Mykolaichuk <alexmix3000@gmail.com>
2022-03-27 21:04:24 +0200

    added public url

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [[ ${VERCEL_ENV} == "production" ]]; then
-    echo "Production deployment with VERCEL_URL: '$VERCEL_URL'";
-    env HUGO_ENABLE_GA="true" hugo -b https://$VERCEL_URL --gc
+    echo "Production deployment with: '$PUBLIC_URL'";
+    env HUGO_ENABLE_GA="true" hugo -b $PUBLIC_URL --gc
 else
     echo "Not production deployment with VERCEL_URL: '$VERCEL_URL'";
     hugo -b https://$VERCEL_URL -D --gc
